f2e82f88f5a6727e701b295bcd2c9ee4718210d1
deciduously <bendlovy@gmail.com>
2017-04-11 17:34:54 -0400

    put xsessionrc back in xinitrc

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,29 @@
 import XMonad
-import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-baseConfig = desktopConfig
+--baseConfig = desktopConfig
 
-main = xmonad def
-     { terminal          = myTerminal
-     , modMask           = myModMask
-     }
+main = do
+  xmproc <- spawnPipe "xmobar"
+
+  xmonad $ defaultConfig
+    { manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , logHook = dynamicLogWithPP xmobarPP
+                  { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "green" "" . shorten 50
+                  }
+    , terminal          = myTerminal
+    , modMask           = myModMask
+    } `additionalKeys`
+    [ ((myModMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+    , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+    , ((0, xK_Print), spawn "scrot")
+    ]
 
 myTerminal          = "urxvt"
 myModMask           = mod4Mask
